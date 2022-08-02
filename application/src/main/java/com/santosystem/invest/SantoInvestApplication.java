ea50a5ffb8c278a446a9f8489522348267f7e4b8
kalangoti <kalangoti@gmail.com>
2022-08-02 18:10:05 -0300

    Project created

---
+++ application/src/main/java/com/santosystem/invest/SantoInvestApplication.java
@@ -0,0 +1,13 @@
+package com.santosystem.invest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantoInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantoInvestApplication.class, args);
+	}
+
+}
---
+++ application/src/test/java/com/santosystem/invest/SantoInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosystem.invest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantoInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
